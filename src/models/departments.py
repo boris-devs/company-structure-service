d84a1309061a1a7408bfc46fd1ccbe27c57909aa
xborismenx <boris.tikhonov.21@gmail.com>
2026-05-16 20:14:22 +0300

    Add unique constraint on `parent_id` and `name` in `Departments` model

--- src/models/departments.py
+++ src/models/departments.py
@@ -2,7 +2,7 @@ from src.models import Base
 
 from datetime import datetime
 from typing import Optional
-from sqlalchemy import ForeignKey, String, DateTime, func
+from sqlalchemy import ForeignKey, String, DateTime, func, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
@@ -10,7 +10,11 @@ class Departments(Base):
 	__tablename__ = "departments"
 
 	id: Mapped[int] = mapped_column(primary_key=True)
-	name: Mapped[str] = mapped_column(String(255), nullable=False)
+	name: Mapped[str] = mapped_column(String(200), nullable=False)
 	parent_id: Mapped[Optional[int]] = mapped_column(ForeignKey("departments.id"), nullable=True)
 	created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
 	parent: Mapped["Departments"] = relationship("Departments", remote_side=[id])
+
+	__table_args__ = (
+		UniqueConstraint("parent_id", "name", name="uq_department_parent_name"),
+	)
