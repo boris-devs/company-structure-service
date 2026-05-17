860ae9ef82dbec85ccae87ee17cfe1a45f37a76a
xborismenx <boris.tikhonov.21@gmail.com>
2026-05-17 13:25:50 +0300

    Add relationships between `Departments` and `Employees` models

--- src/models/departments.py
+++ src/models/departments.py
@@ -1,9 +1,12 @@
 from src.models import Base
 
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List, TYPE_CHECKING
 from sqlalchemy import ForeignKey, String, DateTime, func, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+if TYPE_CHECKING:
+	from src.models.employees import Employees
 
 
 class Departments(Base):
@@ -11,10 +14,16 @@ class Departments(Base):
 
 	id: Mapped[int] = mapped_column(primary_key=True)
 	name: Mapped[str] = mapped_column(String(200), nullable=False)
-	parent_id: Mapped[Optional[int]] = mapped_column(ForeignKey("departments.id"), nullable=True)
+	department_id: Mapped[Optional[int]] = mapped_column(ForeignKey("departments.id"), nullable=True)
 	created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
 	parent: Mapped["Departments"] = relationship("Departments", remote_side=[id])
 
+	children: Mapped[List["Departments"]] = relationship(
+		"Departments",
+		back_populates="parent",
+		lazy="selectin",
+	)
+	employees: Mapped[List["Employees"]] = relationship("Employees", back_populates="department")
 	__table_args__ = (
-		UniqueConstraint("parent_id", "name", name="uq_department_parent_name"),
+		UniqueConstraint("department_id", "name", name="uq_department_parent_name"),
 	)
--- src/models/employees.py
+++ src/models/employees.py
@@ -1,10 +1,13 @@
 from src.models import Base
 
 from datetime import datetime, date
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
 
 from sqlalchemy import ForeignKey, String, DateTime, func, Date
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+if TYPE_CHECKING:
+	from src.models.departments import Departments
 
 
 class Employees(Base):
@@ -16,3 +19,4 @@ class Employees(Base):
 	position: Mapped[str] = mapped_column(String(255), nullable=False)
 	hired_at: Mapped[Optional[date]] = mapped_column(Date, nullable=True)
 	created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
+	department: Mapped["Departments"] = relationship("Departments", back_populates="employees")
